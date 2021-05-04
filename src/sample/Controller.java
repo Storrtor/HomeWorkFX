d89137c9d442c8a5badb0792118e868b464df3ae
Storrtor <storrtor@gmail.com>
2021-05-04 23:48:53 +0300

    Start

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,18 +2,12 @@ package sample;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class Controller {
     StringBuilder builder = new StringBuilder();
-
-    @FXML
-    public ScrollBar scroll;
 
     @FXML
     private ResourceBundle resources;
